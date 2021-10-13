a42ce96b6596a318fa5e12c567e50b9bc590b22b
Morinishi15 <54336828+Morinishi15@users.noreply.github.com>
2021-10-13 13:14:57 -0300

    Atualizando Classe Telecomunicação

--- Telecomunicacao.java
+++ Telecomunicacao.java
@@ -1,4 +1,12 @@
 
 public class Telecomunicacao {
-
+	private String categoria;
+	private String subcategoria;
+	
+	public void registrarSubCategoria(int escolha) {
+		
+	}
+	public void registrarValorDespesa(int valor) {
+		
+	}
 }
