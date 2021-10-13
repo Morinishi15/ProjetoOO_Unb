90558b832c39b412e874bf07c04e0825585f6dc6
Morinishi15 <54336828+Morinishi15@users.noreply.github.com>
2021-10-13 01:10:17 -0300

    Implementação Classe Despesas

---
+++ Despesas.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Despesas {
+	private String nome;
+	private String categoria;
+	public int totalDeDespesasProporcional;
+	public int totalDeDespesasIgualitarias;
+	
+	public void total (double valor) {
+		
+		
+	}
+	public void escolherOTipoDeDivisao(int escolha) {
+	
+		
+	}
+	public void escolherACategoria(int escolhaCategoria) {
+					
+		}
+		
+	}
+
